4d733b9f944bb7ba5203febc907460405e44883b
atanad nedyalkov <atanas.nedyalkov92@gmail.com>
2022-07-22 11:23:56 +0300

    Add project files.

---
+++ 01. Meal Plan/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Meal_Plan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] meal = Console.ReadLine().Split().ToArray();
+            int[] calories = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<string> mealQueue = new Queue<string>(meal);
+            Stack<int> calorieStack = new Stack<int>(calories);
+
+            int mealsEaten = 0;
+
+            while (mealQueue.Count>0 && calorieStack.Count>0)
+            {
+                int currentCalories = calorieStack.Peek();
+                while (currentCalories>0)
+                {
+                string currentMeal = mealQueue.Peek();
+
+                    if (currentMeal == "salad")
+                    {
+                        currentCalories -= 350;
+                        mealQueue.Dequeue();
+                        mealsEaten++;
+                    }
+                    else if (currentMeal == "soup")
+                    {
+                        currentCalories -= 490;
+                        mealQueue.Dequeue();
+                        mealsEaten++;
+                    }
+                    else if (currentMeal == "pasta")
+                    {
+                        currentCalories -= 680;
+                        mealQueue.Dequeue();
+                        mealsEaten++;
+                    }
+                    else if (currentMeal == "steak")
+                    {
+                        currentCalories -= 790;
+                        mealQueue.Dequeue();
+                        mealsEaten++;
+                    }
+                    
+                    if (currentCalories < 0)
+                    {
+                        int leftCals = currentCalories;
+                        calorieStack.Pop();
+                        if (calorieStack.Count == 0)
+                        {
+                            break;
+                        }
+                        currentCalories = calorieStack.Peek();
+                        currentCalories += leftCals;
+                        calorieStack.Pop();
+                        calorieStack.Push(currentCalories);
+                        break;
+
+                    }
+                }
+            }
+
+            if (mealQueue.Count == 0)
+            {
+                Console.WriteLine($"John had {mealsEaten} meals.");
+                Console.WriteLine($"For the next few days, he can eat {string.Join(", ", calorieStack)} calories.");
+            }
+            if (calorieStack.Count == 0)
+            {
+                Console.WriteLine($"John ate enough, he had {mealsEaten} meals.");
+                Console.WriteLine($"Meals left: {string.Join(", ", mealQueue)}.");
+            }
+        }
+          
+    }
+}
---
+++ BlackSmith/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BlackSmith
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //steel - firstinfirstout -- queue
+            //carbon - firstinLastOut -- stack
+            int[] steel = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] carbon = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> steelRessources = new Queue<int>(steel);
+
+            Stack<int> carbonRessources = new Stack<int>(carbon);
+
+            SortedDictionary<string, int> swords = new SortedDictionary<string, int>
+            {
+                {"Gladius" , 0 },
+                {"Shamshir", 0},
+                {"Katana" , 0},
+                {"Sabre", 0},
+                {"Broadsword" , 0}
+            };
+            int totalSwords = 0;
+            while (steelRessources.Count>0 && carbonRessources.Count>0)
+            {
+                int currentSteel = steelRessources.Peek();
+                int currentCarbon = carbonRessources.Peek();
+                int sum = currentCarbon + currentSteel;
+                if (sum == 70)
+                {
+                    swords["Gladius"]++;
+                    totalSwords++;
+                    steelRessources.Dequeue();
+                    carbonRessources.Pop();
+                }
+                else if (sum == 80)
+                {
+                    swords["Shamshir"]++;
+                    totalSwords++;
+                    steelRessources.Dequeue();
+                    carbonRessources.Pop();
+                }
+                else if (sum == 90)
+                {
+                    swords["Katana"]++;
+                    totalSwords++;
+                    steelRessources.Dequeue();
+                    carbonRessources.Pop();
+                }
+                else if (sum == 110)
+                {
+                    swords["Sabre"]++;
+                    totalSwords++;
+                    steelRessources.Dequeue();
+                    carbonRessources.Pop();
+                }
+                else if (sum ==150)
+                {
+
+                    swords["Broadsword"]++;
+                    totalSwords++;
+                    steelRessources.Dequeue();
+                    carbonRessources.Pop();
+                }
+                else
+                {
+                    steelRessources.Dequeue();
+                    currentCarbon += 5;
+                    carbonRessources.Pop();
+                    carbonRessources.Push(currentCarbon);
+
+                }
+
+            }
+            if (totalSwords>=1)
+            {
+                Console.WriteLine($"You have forged {totalSwords} swords.");
+            }
+            else
+            {
+                Console.WriteLine("You did not have enough resources to forge a sword.");
+            }
+            if (steelRessources.Count==0)
+            {
+                Console.WriteLine("Steel left: none");
+            }
+            else
+            {
+                Console.WriteLine($"Steel left:{string.Join(", " , steelRessources)}");
+            }
+            if (carbonRessources.Count == 0)
+            {
+                Console.WriteLine("Carbon left: none");
+            }
+            else
+            {
+                Console.WriteLine($"Carbon left: {string.Join(", " , carbonRessources)}");
+            }
+            foreach (var sword in swords)
+            {
+                if (sword.Value>0)
+                {
+                    Console.WriteLine($"{sword.Key}: {sword.Value}");
+                }
+            }
+        }
+    }
+}
